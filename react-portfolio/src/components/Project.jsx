568ae737777ceb6eef4e54cc53f4511c99d75cca
Marly Valcin <marlyvalcin@gmail.com>
2024-08-09 13:47:58 -0400

    updated the styling for the header, about me, index and porject css files and updated the project component

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,23 +1,29 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Col } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import '../assets/styles/Project.css';
 
 function ProjectCards() {
   return (
-    <Col xs={12} md={4} className="g-2">
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Col>
+    <section>
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card className='card'>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body className='card-body'>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    </section>
   );
 }
 
