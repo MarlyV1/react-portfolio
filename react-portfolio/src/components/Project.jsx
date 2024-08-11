c0c97acef2379663ba96d3c4abce70706857eb02
Marly Valcin <marlyvalcin@gmail.com>
2024-08-10 23:34:48 -0400

    added project images to the assets folder and updated projectInfo for the project cards

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,57 +1,61 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import '../assets/styles/Project.css';
-import background from '../assets/images/background.jpg';
+import devXConnect from '../assets/images/devXConnect.png';
+import raincheck from '../assets/images/raincheck.png';
 import blog from '../assets/images/personal-blog.png';
+import weatherDashboard from '../assets/images/weather-dashboard.png';
+import taskBoard from '../assets/images/task-board.png';
+import noteTaker from '../assets/images/note-taker.png';
 
 function ProjectCards() {
 
   const projectInfo = [
-    {
-      id: 'title1',
-      image: 'url',
-      title: 'title1',
-      deployedApp: 'link',
-      repo: 'repo link'
+     {
+      id: 'devXConnect',
+      image: devXConnect,
+      title: 'devXConnect()',
+      deployedApp: 'https://dev-x-connect1.onrender.com/',
+      repo: 'https://github.com/MarlyV1/dev-x-connect1'
     },
     {
-      id: 'title2',
-      image: 'url',
-      title: 'title2',
-      deployedApp: 'link',
-      repo: 'repo link'
+      id: 'raincheck',
+      image: raincheck,
+      title: 'RainCheck',
+      deployedApp: 'https://matt-carpenter-12.github.io/raincheck-project/',
+      repo: 'https://github.com/MarlyV1/raincheck-project'
     },
     {
-      id: 'title3',
-      image: 'url',
-      title: 'title3',
-      deployedApp: 'link',
-      repo: 'repo link'
+      id: 'personal-blog',
+      image: blog,
+      title: 'Personal Blog',
+      deployedApp: 'https://marlyv1.github.io/personal-blog/',
+      repo: 'https://github.com/MarlyV1/personal-blog'
     },
     {
-      id: 'title4',
-      image: 'url',
-      title: 'title4',
-      deployedApp: 'link',
-      repo: 'repo link'
+      id: 'weather-dashboard',
+      image: weatherDashboard,
+      title: 'Weather Dashboard',
+      deployedApp: 'https://marlyv1.github.io/weather-dashboard-server-side-APIs/',
+      repo: 'https://github.com/MarlyV1/weather-dashboard-server-side-APIs'
     },
+   
     {
-      id: 'title5',
-      image: 'url',
-      title: 'title5',
-      deployedApp: 'link',
-      repo: 'repo link'
+      id: 'taask-board',
+      image: taskBoard,
+      title: 'Third-Party-APIs-Task-Board',
+      deployedApp: 'https://marlyv1.github.io/third-party-APIs-task-board/',
+      repo: 'https://github.com/MarlyV1/third-party-APIs-task-board'
     },
-    {
-      id: 'title6',
-      image: 'url',
-      title: 'title6',
-      deployedApp: 'link',
-      repo: 'repo link'
-    },
+     {
+      id: 'note-taker',
+      image: noteTaker,
+      title: 'Note Taker',
+      deployedApp: 'https://note-taker-gock.onrender.com/',
+      repo: 'https://github.com/MarlyV1/note-taker'
+    }
   ]
 
 
@@ -63,13 +67,10 @@ function ProjectCards() {
         {/* <Col xs={1} md={2} className="g-2"> */}
           {projectInfo.map((project) => (
             <Card className='card' key={project.id} style={{ width: '32rem' }}>
-              <Card.Img variant="top" src={blog} />
+              <Card.Img variant="top" src={project.image} />
               <Card.Body>
                 <Card.Title>{project.title}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
+                
                 <Button variant="primary">Go somewhere</Button>
               </Card.Body>
             </Card>
