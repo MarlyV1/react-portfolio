a8825fb4709c408f97ce66529e77a8ba1f96d1fe
Marly Valcin <marlyvalcin@gmail.com>
2024-08-10 23:02:18 -0400

    updated the contact form

--- react-portfolio/src/components/Contact.jsx
+++ react-portfolio/src/components/Contact.jsx
@@ -1,22 +1,28 @@
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import '../assets/styles/Contact.css';
 
 function Contact() {
-  return (
-    <section>
-    <h2 className='sectionHeading'>Contact</h2>
-    <Form className='contactForm'>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-    </Form>
-    </section>
-  );
+    return (
+        <section>
+            <h2 className='sectionHeading'>Contact</h2>
+            <Form className='contactForm'>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Name" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" rows={3} required/>
+                </Form.Group>
+                <Button type="submit">Submit</Button>
+            </Form>
+        </section>
+    );
 }
 
 export default Contact;
